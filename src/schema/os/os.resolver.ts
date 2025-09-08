45cc43586a6a46d351008f04578f7d65a4e6142c
CIEP <ciepcgmpvh@gmail.com>
2025-09-08 13:14:12 -0400

    Update os.resolver.ts

--- src/schema/os/os.resolver.ts
+++ src/schema/os/os.resolver.ts
@@ -1,20 +1,9 @@
+// src/schema/os/os.resolver.ts
 import { FastifyInstance } from 'fastify'
 
 const osResolvers = () => ({
-  // Query: {
-  //   // ordensServico: async () => {
-  //   //   return app.prisma.ordem_servico.findMany({
-  //   //     include: { veiculo: true, fornecedor: true, cliente: true },
-  //   //     orderBy: { data: 'desc' }
-  //   //   })
-  //   // },
-  //   // ordemServico: async (_: any, { id }: { id: number }) => {
-  //   //   return app.prisma.ordem_servico.findUnique({
-  //   //     where: { id },
-  //   //     include: { veiculo: true, fornecedor: true, cliente: true }
-  //   //   })
-  //   // }
-  // }
+  // Deixe este arquivo vazio por enquanto, até que as queries de Ordem de Serviço sejam implementadas.
+  // Não defina um objeto Query vazio aqui.
 })
 
-export default osResolvers;
+export default osResolvers
