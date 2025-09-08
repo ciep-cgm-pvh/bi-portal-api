45ce0acabc35174af42d11f8eb6bb5d1c6783bca
CIEP <ciepcgmpvh@gmail.com>
2025-09-08 13:08:34 -0400

    Update diarias.resolver.ts

--- src/schema/diarias/diarias.resolver.ts
+++ src/schema/diarias/diarias.resolver.ts
@@ -2,13 +2,13 @@ import { FastifyInstance } from 'fastify';
 import { loadDiarias } from '../../data/loadDiarias';
 
 const diariasResolvers = () => ({
-  Query: {
-    diarias: async () => {
-      const data = await loadDiarias();
-      return data;
+  //Query: {
+    //diarias: async () => {
+      //const data = await loadDiarias();
+      //return data;
       // return context.prisma.diaria.findMany(); // exemplo com Prisma
-    },
-  },
+   // },
+ // },
 });
 
 export default diariasResolvers;
