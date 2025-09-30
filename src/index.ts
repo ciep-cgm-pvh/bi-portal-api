f900dd84f03ab187e378e147685a660665e517e8
Clara Helena <clarahelenasena@gmail.com>
2025-09-30 09:11:41 -0400

    [update]: cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 // src/index.ts
 import { buildServer } from './server'
 
+const PORT = Number(process.env.PORT)
+
 buildServer().then((app) => {
-  app.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
+  app.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
     if (err) {
       app.log.error(err)
       process.exit(1)
     }
-    console.log('🚀 Server ready at http://localhost:3000/graphiql')
+    console.log(`🚀 Server ready at http://localhost:${PORT}/graphiql`)
   })
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,58 @@
 // src/server.ts
 import Fastify from 'fastify';
 import mercurius from 'mercurius';
-import prismaPlugin from './plugins/prisma';
 import { buildSchema } from './schema/index';
 import cors from '@fastify/cors';
+import fastifyHelmet from '@fastify/helmet';
+import dotenv from 'dotenv';
+import fastifyRateLimit from '@fastify/rate-limit';
+
+dotenv.config();
 
 export async function buildServer() {
   const app = Fastify();
 
-  // await app.register(prismaPlugin);
+  const allowedOrigins = (process.env.FRONTEND_URLS || '').split(',');
+  const isDev = process.env.NODE_ENV !== 'production';
 
   app.register(cors, {
-    origin: '*', // ou pode ser um array ou função para controlar quais origens permitir
+    origin: isDev ? ["http://localhost:5173", allowedOrigins] : allowedOrigins,
   });
+
+  await app.register(fastifyRateLimit, {
+    max: 50,       // Máx. requisições
+    timeWindow: "1 minute", // Janela de tempo
+    errorResponseBuilder: (req, context) => {
+      return {
+        code: "TOO_MANY_REQUESTS",
+        message: `Você fez muitas requisições. Tente novamente em ${context.after}`,
+      };
+    },
+  })
   
   const schema = await buildSchema(app); // <-- chama a função com `app`
+
+  await app.register(fastifyHelmet, {
+    contentSecurityPolicy: isDev
+      ? false
+      : {
+        directives: {
+          defaultSrc: [ "'self'" ],
+          scriptSrc: [ "'self'" ],
+          styleSrc: [ "'self'" ],
+          imgSrc: [ "'self'" ],
+          fontSrc: [ "'self'" ],
+          objectSrc: [ "'none'" ],
+          frameAncestors: [ "'self'" ],
+          upgradeInsecureRequests: [],
+        },
+      },
+  });
 
   app.register(mercurius, {
     schema,
     context: () => ({}), // contexto vazio, você pode adicionar o Prisma aqui se necessário
-    graphiql: true,
+    graphiql: isDev, // habilita o GraphiQL apenas em desenvolvimento
   });
 
   return app;
